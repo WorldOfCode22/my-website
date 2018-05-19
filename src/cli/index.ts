79f51acbd664ee1b329e723d79dfea2bef3caf35
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-19 18:35:41 -0400

    entry point to cli

---
+++ src/cli/index.ts
@@ -0,0 +1,11 @@
+// cli entry point
+import { CommandLineInterface } from './classes/command-line-interface'
+import { CommandNotGiven } from './classes/errors/user-input-errors/command-not-given'
+try {
+  let cli = new CommandLineInterface()
+} catch (e) {
+  if (e instanceof CommandNotGiven) {
+    console.log(e)
+    console.log('A command must be provided to use this application. Please try again with a command provided')
+  }
+}
