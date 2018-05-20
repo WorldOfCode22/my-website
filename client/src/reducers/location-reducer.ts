bcb677d1ec113855f28a2756a16d96964cf1c0b7
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-20 14:01:08 -0400

    added mock to location reducer

--- client/src/reducers/location-reducer.ts
+++ client/src/reducers/location-reducer.ts
@@ -1,9 +1,23 @@
+import { IState } from "../App";
+
 export interface IApplicationLocation {
   location: string
+  changeLocation: (path: string) => void
 }
 
-export function getLocationDefaults(): IApplicationLocation {
+export const mockApplicationLocation: IApplicationLocation = {
+  // tslint:disable-next-line:no-empty
+  changeLocation: () => {},
+  location: '/',
+}
+
+export function getLocationDefaults(comp: React.Component<{}, IState>): IApplicationLocation {
   return {
+    changeLocation: (path: string) => {
+      const {location} = comp.state
+      location.location = path
+      comp.setState({location})
+    },
     location: "/"
   }
 }
