c9a9aff6ff55144887b6e3d44b717cb9b5061999
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-20 16:56:15 -0400

    graphql init

--- src/api/classes/express-application.ts
+++ src/api/classes/express-application.ts
@@ -1,11 +1,20 @@
 import express from 'express'
+import expressGraphQL from 'express-graphql'
 import ConfigurationManager from '../classes/configuration-manager'
+import Schema from '../gql/schema'
 
 export default class ExpressApplication {
   private _application: express.Express = express()
-  // tslint:disable-next-line:no-empty
-  constructor () {}
+  constructor () {
+    this.setup()
+  }
 
+  private setup () {
+    this._application.use('/graphql', expressGraphQL({
+      graphiql: true,
+      schema: Schema
+    }))
+  }
   get application (): express.Express {
     return this._application
   }
---
+++ src/api/gql/root.ts
@@ -0,0 +1,8 @@
+import { GraphQLObjectType } from 'graphql'
+
+const RootQuery = new GraphQLObjectType({
+  fields: {},
+  name: 'Root_Query'
+})
+
+export default RootQuery
