11874c7b10fa365dcc3e7d37ccac4366c4bec335
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-19 23:09:00 -0400

    added navbar to header

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Jumbotron} from "reactstrap";
 import { ApplicationContext } from "../../App";
+import Navbar from "../navbar/navbar";
 import './header.css';
 
 const Header: React.SFC = () => (
@@ -9,8 +10,9 @@ const Header: React.SFC = () => (
       <div>
         <Jumbotron className="header">
           <h1 className="text-center">{state.page.heading}</h1>
-          <h3 className="text-center">{state.page.subHeading}</h3>
+          <h5 className="text-center">{state.page.subHeading}</h5>
         </Jumbotron>
+        <Navbar />
       </div>
     )}
   </ApplicationContext.Consumer>
