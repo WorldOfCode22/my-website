80dfaee05b2d9b411cec3bffc33db1cb31ba7529
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-21 20:21:32 -0400

    added node mailer to root query

--- src/api/gql/root.ts
+++ src/api/gql/root.ts
@@ -1,7 +1,20 @@
-import { GraphQLObjectType } from 'graphql'
+import { GraphQLObjectType, GraphQLNonNull, GraphQLString } from 'graphql'
+import NodeMailerType from './objects/nodemailer'
+import NodeMailerResolver from './resolvers/node-mailer-resolver'
 
 const RootQuery = new GraphQLObjectType({
-  fields: {},
+  fields: {
+    NodeMailer: {
+      args: {
+        to: { type: new GraphQLNonNull(GraphQLString) },
+        type: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve (parentVal, args) {
+        return NodeMailerResolver(args)
+      },
+      type: NodeMailerType
+    }
+  },
   name: 'Root_Query'
 })
 
