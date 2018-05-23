3c89b4893686506e213cc69e06d4b798464b45db
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-22 20:27:09 -0400

    added detail to node mailer form

--- client/src/components/skills-components/node-mailer/node-mailer.tsx
+++ client/src/components/skills-components/node-mailer/node-mailer.tsx
@@ -1,14 +1,42 @@
 import * as React from 'react'
+import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 import { ApplicationContext } from '../../../App';
 
 const NodeMailer = () => (
   <ApplicationContext.Consumer>
-    {state => (
-      <div>
-        <h1>Node Mailer Test Page</h1>
-      </div>
-    )}
+    {state => {
+      if (state.gql.data.empty) {
+        return <MailerTestForm formData={state.mailer}/>
+      } else {
+        return <MailerDone />
+      }
+    }}
   </ApplicationContext.Consumer>
 )
 
+interface IFormProps {
+  formData: {}
+}
+const MailerTestForm = (props: IFormProps) => (
+  <Col className="text-center">
+    <Form>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </FormGroup>
+      <FormGroup>
+        <Label>Type of Email</Label>
+        <Input type="select">
+          <option>Show of ability to use NodeMailer</option>
+          <option>Contact Me Verification</option>
+        </Input>
+      </FormGroup>
+      <Button color="primary">Send Email</Button>
+    </Form>
+  </Col>
+)
+
+const MailerDone = () => (
+  <h1>Mail Sent</h1>
+)
 export default NodeMailer
