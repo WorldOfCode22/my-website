f9389ab6790a1b4695b95db6379d6fd4174e5164
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-22 20:24:37 -0400

    added miler to app component and refactored layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import {Container} from 'reactstrap';
+import {Col, Container, Row} from 'reactstrap';
 import './App.css';
-import Header from "./components/header/header"
+import Header from "./components/header/header";
+import Navbar from "./components/navbar/navbar";
 import Router from './components/router/router';
 import {getGQLDefaults, IGQLCommandStatus} from "./reducers/gql-reducer";
 import {getLocationDefaults, IApplicationLocation, mockApplicationLocation } from './reducers/location-reducer';
+import { getDefaultMailer, IMailer } from './reducers/mailer-reducer';
 import {getDefaultNavigation, INavigation, mockNavigation} from "./reducers/navigation-reducer"
 import { getHomePage, IPage } from './reducers/page-reducer';
 
+
 export interface IState {
-  gql: IGQLCommandStatus
+  gql: IGQLCommandStatus,
+  mailer: IMailer,
   page: IPage
   navigation: INavigation
   location: IApplicationLocation
@@ -18,6 +22,7 @@ export interface IState {
 export const ApplicationContext = React.createContext({
   gql: getGQLDefaults(),
   location: mockApplicationLocation,
+  mailer: getDefaultMailer(),
   navigation: mockNavigation(),
   page: getHomePage(),
 })
@@ -26,6 +31,7 @@ class App extends React.Component<{}, IState> {
   public state: IState = {
     gql: getGQLDefaults(),
     location: getLocationDefaults(this),
+    mailer: getDefaultMailer(),
     navigation: getDefaultNavigation(this),
     page: getHomePage(),
   }
@@ -34,7 +40,12 @@ class App extends React.Component<{}, IState> {
       <ApplicationContext.Provider value={this.state}>
         <Container fluid={true}>
           <Header />
-          <Router />
+          <Row>
+          <Col xs={3} sm={3} md={3} lg={3} xl={3}>
+            <Navbar />
+          </Col>
+              <Router />
+          </Row>
         </Container>
       </ApplicationContext.Provider>
     );
