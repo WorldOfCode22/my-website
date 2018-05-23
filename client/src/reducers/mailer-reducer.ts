f932398d50cb279dccbffa12fd8cc993c8eb3844
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-22 20:49:50 -0400

    add detail to mailer reducer

--- client/src/reducers/mailer-reducer.ts
+++ client/src/reducers/mailer-reducer.ts
@@ -1,9 +1,29 @@
+import { IState } from "../App";
+
 export interface IMailer {
-  to: string
+  to: string,
+  type: string,
+  sendMail: (to: string, type: string) => void
 }
 
-export function getDefaultMailer () {
+export const mockMailerData: IMailer = {
+  // tslint:disable-next-line:no-empty
+  sendMail: (to: string, type: string) => {},
+  to: "",
+  type: ""
+}
+
+export function getDefaultMailer (comp: React.Component<{}, IState>) {
   return {
-    to: ""
+    sendMail: (to: string, type: string) => {
+      const { gql, location, mailer} = comp.state;
+      mailer.to = to;
+      mailer.type = type;
+      gql.loading = true;
+      location.location = '/loading';
+      comp.setState({gql, mailer});
+    },
+    to: "",
+    type: ""
   }
 }
