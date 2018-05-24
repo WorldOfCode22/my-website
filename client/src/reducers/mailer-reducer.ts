4c4c2a0b252a5f0f58c08dee27291c2ad2e6ed63
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-24 17:52:14 -0400

    refactored mailer-reducer for event bassed state changes

--- client/src/reducers/mailer-reducer.ts
+++ client/src/reducers/mailer-reducer.ts
@@ -1,35 +1,10 @@
-import { IState } from "../App";
-import GQLCommand from "../components/gql-command/gql-command";
-
 export interface IMailer {
   to: string,
   type: string,
-  sendMail: (to: string, type: string) => void
 }
 
-export const mockMailerData: IMailer = {
-  // tslint:disable-next-line:no-empty
-  sendMail: (to: string, type: string) => {},
-  to: "",
-  type: ""
-}
-
-export function getDefaultMailer (comp: React.Component<{}, IState>) {
+export function getDefaultMailer () {
   return {
-    sendMail: (to: string, type: string) => {
-      const { gql, location, mailer} = comp.state;
-      mailer.to = to;
-      mailer.type = type;
-      gql.active = true;
-      gql.loading = true;
-      GQLCommand({local: location.location, query: `{
-        NodeMailer(to:"${to}", type:"${type}"){
-          emailSent
-        }
-      }`, state: comp.state})
-      location.location = '/loading';
-      comp.setState({gql, location, mailer});
-    },
     to: "",
     type: ""
   }
