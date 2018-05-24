177f2aa4b4f8e27b6131fd49451cb60221ece301
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-24 17:51:45 -0400

    added errors key to gql-reducers

--- client/src/reducers/gql-reducer.ts
+++ client/src/reducers/gql-reducer.ts
@@ -1,6 +1,7 @@
 export interface IGQLCommandStatus {
   active: boolean,
   data: {},
+  errors: [{}] | undefined
   loading: boolean
 }
 
@@ -8,6 +9,7 @@ export function getGQLDefaults (): IGQLCommandStatus {
   return {
     active: false,
     data: {},
+    errors: undefined,
     loading: false
   }
 }
