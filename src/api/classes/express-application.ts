aa1e082d9e6ed21485a76cea017450d6ccc10775
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-25 19:52:25 -0400

    changs made to application for socket.io

--- src/api/classes/express-application.ts
+++ src/api/classes/express-application.ts
@@ -3,15 +3,20 @@ import expressGraphQL from 'express-graphql'
 import ConfigurationManager from '../classes/configuration-manager'
 import Schema from '../gql/schema'
 import cors from 'cors'
+import { createServer, Server } from 'http'
+import io from 'socket.io'
+import IOManager from './io-manager'
 
 export default class ExpressApplication {
   private _application: express.Express = express()
+  private _server: Server = new Server(this._application)
+  private _io = new IOManager(io(this._server))
   constructor () {
     this.setup()
   }
 
   private setup () {
-    this._application.use(cors())
+    this._application.use(cors({ origin: 'http://localhost:3000' }))
     this._application.use('/graphql', expressGraphQL({
       graphiql: true,
       schema: Schema
@@ -19,5 +24,9 @@ export default class ExpressApplication {
   }
   get application (): express.Express {
     return this._application
+  }
+
+  get server (): Server {
+    return this._server
   }
 }
