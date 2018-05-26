ffdb2d3bb741ab4906e2d9b8de5718c46db0e611
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-26 18:38:51 -0400

    added reducer for state.socket

--- client/src/reducers/socket-reducer.ts
+++ client/src/reducers/socket-reducer.ts
@@ -1,4 +1,5 @@
 import * as io from "socket.io-client";
+import { createAction } from "../action-reducers";
 import { IState } from "../App";
 import { getEnvironment } from "./env";
 
@@ -8,8 +9,20 @@ export interface ISocket {
 
 export function getSocketDefaults (comp: React.Component<{}, IState>): ISocket {
   if (getEnvironment().mode === "DEVELOPMENT") {
-    return {socket: io("http://localhost:4000")}
+    return setUpSocket(comp, io("http://localhost:4000"))
   } else {
-    return {socket: io("/")}
+    return setUpSocket(comp, io("/")) 
   }
+}
+
+export function setUpSocket (comp: React.Component<{}, IState>, socket: SocketIOClient.Socket): ISocket {
+  socket.on("SERVER RECEIVED USER MESSAGE", () => {
+    comp.state.actionListener(createAction("SERVER RECEIVED YOUR MESSAGE", {}))
+  })
+
+  socket.on("SERVER RESPONDED TO USER MESSAGE", (serverMessage: string) => {
+    comp.state.actionListener(createAction("SERVER RESPONDED TO YOUR MESSAGE", {chat: {serverMessage}}))
+  })
+
+  return {socket}
 }
