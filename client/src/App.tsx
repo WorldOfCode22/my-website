4aad53bc6495e3f3f37cc4ac29d96d0476473cc7
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-26 18:36:58 -0400

    added chat to state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,18 +5,20 @@ import './App.css';
 import Header from "./components/header/header";
 import Navbar from "./components/navbar/navbar";
 import Router from './components/router/router';
+import getChatDefaults, { IChat } from './reducers/chat-reducer';
 import { getEnvironment, IEnvironment } from './reducers/env';
 import {getGQLDefaults, IGQLCommandStatus} from "./reducers/gql-reducer";
 import {getLocationDefaults, IApplicationLocation} from './reducers/location-reducer';
 import { getDefaultMailer, IMailer } from './reducers/mailer-reducer';
 import {getDefaultNavigation, INavigation} from "./reducers/navigation-reducer"
 import { getHomePage, IPage } from './reducers/page-reducer';
-import { getSocketDefaults, ISocket } from './reducers/socket-reducer';
+import { getSocketDefaults, ISocket} from './reducers/socket-reducer';
 
 
 export interface IState {
   // tslint:disable-next-line:ban-types
   actionListener: (action: IAction) => void,
+  chat: IChat,
   env: IEnvironment,
   gql: IGQLCommandStatus,
   mailer: IMailer,
@@ -29,6 +31,7 @@ export interface IState {
 export const ApplicationContext = React.createContext({
   // tslint:disable-next-line:no-empty
   actionListener: (action: IAction) => {},
+  chat: getChatDefaults(),
   env: getEnvironment(),
   gql: getGQLDefaults(),
   location: getLocationDefaults(),
@@ -42,6 +45,7 @@ class App extends React.Component<{}, IState> {
     actionListener: (action: IAction) => {
       actionListener(this, action)
     },
+    chat: getChatDefaults(),
     env: getEnvironment(),
     gql: getGQLDefaults(),
     location: getLocationDefaults(),
@@ -50,6 +54,7 @@ class App extends React.Component<{}, IState> {
     page: getHomePage(),
     socket: getSocketDefaults(this)
   }
+
   public render() {
     return (
       <ApplicationContext.Provider value={this.state}>
@@ -59,7 +64,10 @@ class App extends React.Component<{}, IState> {
           <Col xs={3} sm={3} md={3} lg={3} xl={3}>
             <Navbar />
           </Col>
+          <Col xs={6} sm={6} md={6} lg={6} xl={6}>
               <Router />
+          </Col>
+          <Col />
           </Row>
         </Container>
       </ApplicationContext.Provider>
