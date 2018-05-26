f3d96db9acb6714e11f4a68f12fa4a8ffa2161bd
worldofcode22 <gwaltneys2@winthrop.edu>
2018-05-26 18:37:36 -0400

    added chat listener to action index

--- client/src/action-reducers/index.ts
+++ client/src/action-reducers/index.ts
@@ -1,4 +1,5 @@
 import { IState } from "../App";
+import chatActionListener from "./chat-action-reducer";
 import gqlActionListener from "./gql-action-reducer";
 import locationActionListener from "./location-action-reducer";
 import mailerActionListener from "./mailer-action-reducer";
@@ -8,6 +9,11 @@ import pageActionListener from "./page-action-reducer";
 export interface IAction {
   type: string,
   payload: {
+    chat?: {
+      input?: string,
+      message?: string,
+      serverMessage?: string
+    },
     gql?: {
       data?: {},
       errors?: [{}] | undefined
@@ -33,19 +39,21 @@ function actionListener(comp: React.Component<{}, IState>,action: IAction) {
   if (comp.state.env.mode === "DEVELOPMENT"){
     // tslint:disable-next-line:no-console
     console.log({actionCalled: action, currentState: comp.state})
+    chatActionListener(comp, action);
     locationActionListener(comp, action);
     navigationActionListener(comp, action);
     gqlActionListener(comp, action);
     mailerActionListener(comp, action);
-    pageActionListener(comp, action)
+    pageActionListener(comp, action);
     // tslint:disable-next-line:no-console
-    console.log({actionCalled: action, newState: comp.state})
+    console.log({actionCalled: action, newState: comp.state});
   } else {
+    chatActionListener(comp, action);
     locationActionListener(comp, action);
     navigationActionListener(comp, action);
     gqlActionListener(comp, action);
     mailerActionListener(comp, action);
-    pageActionListener(comp, action)
+    pageActionListener(comp, action);
   }
 }
 
